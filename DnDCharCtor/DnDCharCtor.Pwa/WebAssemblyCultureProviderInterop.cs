16b3896a7f5b7100f1de8091c077d5896fb234b8
Stephan Joneleit <stephan.joneleit@develappers.de>
2024-11-26 17:23:35 +0100

    Update: Use internal API-Calls & Unsafe-Code to load all Language-Resources at Startup;

---
+++ DnDCharCtor/DnDCharCtor.Pwa/WebAssemblyCultureProviderInterop.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace DnDCharCtor.Pwa;
+
+// https://github.com/dotnet/aspnetcore/issues/50361#issuecomment-1699512477
+// https://github.com/dotnet/runtime/issues/98415#issuecomment-1945859860
+internal partial class WebAssemblyCultureProviderInterop
+{
+    // Unfortunally, JSImport requires "AllowUnsafeBlocks" in csproj
+    [JSImport("INTERNAL.loadSatelliteAssemblies")]
+    public static partial Task LoadSatelliteAssemblies(string[] culturesToLoad);
+}
