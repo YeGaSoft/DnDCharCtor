2e1aa4ae6636293c645fd2a85f50f0944fcc8b56
Stephan Joneleit <stephan.joneleit@develappers.de>
2024-12-02 12:36:01 +0100

    Remove: Unused code;

--- DnDCharCtor/DnDCharCtor.Pwa/wwwroot/startup.js
+++
@@ -1,8 +0,0 @@
-﻿// https://github.com/dotnet/runtime/issues/98415#issuecomment-2031216152
-Blazor.start({
-    webAssembly: {
-        configureRuntime: dotnet => {
-            dotnet.withConfig({ loadAllSatelliteResources: true });
-        }
-    }
-});
