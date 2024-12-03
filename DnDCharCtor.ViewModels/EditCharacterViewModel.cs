4240af5df91a558ca74c320e0d14d7cd2fd2bdcb
Stephan Joneleit <StephanJoneleit@outlook.de>
2024-12-03 21:22:35 +0100

    Linter;

--- DnDCharCtor.ViewModels/EditCharacterViewModel.cs
+++ DnDCharCtor.ViewModels/EditCharacterViewModel.cs
@@ -93,8 +93,8 @@ public partial class EditCharacterViewModel : ObservableValidator, IValidateable
 
     public bool HasUnsavedChanges()
     {
-        var chacater = CharacterViewModelToEdit.ToCharacter();
-        var hasChanges = chacater != Character.Empty;
+        var character = CharacterViewModelToEdit.ToCharacter();
+        var hasChanges = character != Character.Empty;
         var isUnsaved = IsSaved is false;
         return hasChanges && isUnsaved;
     }
